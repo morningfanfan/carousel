2a35d5c4d0cbbbf66b633c64150a070c4f42ef53
Zhifan Lan <everydaysiling@gmail.com>
2016-07-23 17:49:53 +0800

    try

--- src/carousel.jsx
+++ src/carousel.jsx
@@ -1,18 +1,51 @@
 
-var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 var React = require('react');
+//var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
-var ImageCarousel = React.createClass({
+
+//var ImageCarousel = React.createClass({
+ // propTypes: {
+   // imageSrc:" ../img/1.png"
+ // },
+  //render: function() {
+   // return (
+  //    <div>
+   //     <ReactCSSTransitionGroup transitionName="carousel" transitionEnterTimeout={300} transitionLeaveTimeout={300}>
+   //       <img src={this.props.imageSrc} key={this.props.imageSrc} />
+  //      </ReactCSSTransitionGroup>
+ //     </div>
+ //   );
+//  }
+//});
+var ImageTrasition = React.createClass({
   propTypes: {
-    imageSrc: "img/1.png"
+    imageSrc:['../img/1.png','../img/1.png'],
+    width:window.innerWidth
+  },
+  getInitialState: {
+    flag = 0
+  },
+  handleClickLeft: function() {
+    $("div#showing").animate({
+      left:'-=this.props.width'
+    },{
+      speed:200,
+      queue:false
+    });
+    $("div#come_right").animate({
+      left:'-=this.props.width'
+    },{
+      speed:200,
+      queue:false
+    });
   },
   render: function() {
-    return (
-      <div>
-        <ReactCSSTransitionGroup transitionName="carousel" transitionEnterTimeout={300} transitionLeaveTimeout={300}>
-          <img src={this.props.imageSrc} key={this.props.imageSrc} />
-        </ReactCSSTransitionGroup>
-      </div>
-    );
+  <div id="carousel">
+    <div id="come_left" img={imageSrc[flag-1]}></div>
+    <div id="showing" img={imageSrc[flag]}></div>
+    <div id="come_right" img={imageSrc[flag+1]}></div>
+      <div id="leftButton" onClick={handleClickLeft}></div>
+      <div id="rightButton"></div>
+  </div>
   }
-});
+})
